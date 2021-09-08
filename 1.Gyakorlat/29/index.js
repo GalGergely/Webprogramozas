4db39868ac25cb8a6bf5ec59e7742f7d5a6fbfbb
GalGergely <galgergely252@gmail.com>
2021-09-08 15:51:05 +0200

    asd

--- 1.Gyakorlat/29/index.js
+++ 1.Gyakorlat/29/index.js
@@ -1,4 +1,4 @@
-const temperatures = [12.3, 23.4, 11.2, -4.5, -3];
+const temperatures = [12.3, 23.4, 11.2];
 function fagyottnapok(temperatures) {
  const out = []
  for(let i = 0; i < temperatures.length; i++)
@@ -8,8 +8,17 @@ function fagyottnapok(temperatures) {
     }
 return out;
 }
+//na ezt nem csinaljuk elvileg
 console.log("Szia Bence");
 console.log(fagyottnapok(temperatures) );
 console.log(temperatures.filter(t => t < 0) );
 console.log(temperatures.map(t => `${t} C`) );
-console.log(temperatures.reduce((s, t) => s+t, 0) );
+console.log(temperatures.reduce((s, t) => s + t, 0) / temperatures.length);
+console.log(temperatures.reduce((max, t) => t > max ? t : max, -Infinity) );
+console.log(Math.max(...temperatures) );
+console.log(temperatures.filter(t => t < 20).length);
+console.log(temperatures.reduce((count, t) => t < 20 ? count+1 : count+0, 0));
+console.log(temperatures.some(t => t > 40) );
+
+//egyeni gyakorlas
+console.log(temperatures.some(t => temperatures.filter(t => t < 0).length) < 0);
